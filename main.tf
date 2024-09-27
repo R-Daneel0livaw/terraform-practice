67edbdcaaced4ccf398954daa2b00ee3f2773f7f
erich <eric.hine2214@gmail.com>
2024-09-27 17:46:08 -0400

    Hook AWS up to terraform and create first bucket.

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-example-bucket"
+resource "aws_s3_bucket" "example-bucket" {
+  bucket = "bucket1-mg"
 }
