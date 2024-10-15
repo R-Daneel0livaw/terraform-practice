5cc7bb32a12887bdc3b812385dedce763a7c57e6
erich <eric.hine2214@gmail.com>
2024-10-15 17:43:56 -0400

    move foundational resources into foundation main.tf

--- modules/global-constants/outputs.tf
+++ modules/global-constants/outputs.tf
@@ -2,3 +2,7 @@ output "bucket_name" {
   description = "The name of the S3 bucket"
   value       = "build-raw-data"
 }
+
+output "directories" {
+  value = ["inbound", "waiting", "completed", "archived"]
+}
--- projects/project1/test/foundation/main.tf
+++ projects/project1/test/foundation/main.tf
@@ -8,8 +8,25 @@ terraform {
   }
 }
 
+
+module "constants" {
+  source = "../../../../modules/global-constants"
+}
+
+module "sqs_queue" {
+  source     = "../../../../modules/sqs"
+  queue_name = "waiting"
+}
+
 resource "aws_s3_bucket" "foundation_bucket" {
   bucket = "my-foundation-bucket"
+}
+
+
+resource "aws_s3_object" "directories" {
+  for_each = toset(module.constants.directories)
+  bucket   = aws_s3_bucket.foundation_bucket.bucket
+  key      = "${each.value}/"
 }
 
 resource "aws_iam_role" "lambda_role" {
