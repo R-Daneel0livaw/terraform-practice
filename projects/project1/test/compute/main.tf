5b67b85592e2410a7ae9a62597f1dc30c17847c7
erich <eric.hine2214@gmail.com>
2024-10-17 17:32:15 -0400

    Move resources to compute stage

--- projects/project1/test/compute/main.tf
+++ projects/project1/test/compute/main.tf
@@ -15,33 +15,44 @@ data "terraform_remote_state" "foundation" {
   }
 }
 
-resource "aws_lambda_function" "lambda" {
-  function_name = "my-lambda-function"
-  s3_bucket     = data.terraform_remote_state.foundation.outputs.bucket_arn  
-  role = data.terraform_remote_state.foundation.outputs.lambda_role_arn
+module "constants" {
+  source = "../../../../modules/global-constants"
 }
 
-# resource "aws_lambda_function" "my_lambda" {
-#   function_name = "my-lambda-function"
-#   s3_bucket     = data.terraform_remote_state.foundation.outputs.lambda_bucket_arn
-#   s3_key        = "lambda-code.zip"
-#   role          = data.terraform_remote_state.foundation.outputs.lambda_role_arn
+resource "aws_lambda_function" "lambda" {
+  function_name = "bucket1-lambda1"
+  role          = data.terraform_remote_state.foundation.outputs.lambda_role_arn
+  handler       = "bucket1_lambda1.lambda_handler"
+  runtime       = "python3.9"
+  filename      = data.archive_file.lambda_zip[each.key].output_path
 
-#   handler = "index.handler"
-#   runtime = "nodejs14.x"
-# }
+  environment {
+    variables = {
+      BUCKET_NAME = module.constants.bucket_name
+    }
+  }
+}
 
-# resource "aws_iam_role_policy" "lambda_s3_policy" {
-#   role = data.terraform_remote_state.foundation.outputs.lambda_role_arn
+resource "aws_iam_role_policy" "lambda_s3_policy" {
+  role = data.terraform_remote_state.foundation.outputs.lambda_role_arn
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = ["s3:GetObject"],
-#         Effect = "Allow",
-#         Resource = "${data.terraform_remote_state.foundation.outputs.lambda_bucket_arn}/*"
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = ["s3:GetObject", "s3:ListBucket"]
+        Effect = "Allow"
+        Resource = [
+          "${data.terraform_remote_state.foundation.outputs.bucket_arn}",
+          "${data.terraform_remote_state.foundation.outputs.bucket_arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambda_code/bucket1_lambda1.py"
+  output_path = "${path.module}/bucket1-lambda1.zip"
+}
