80a19c817c46b50db7043bbecf4c94a00de9f27d
erich <eric.hine2214@gmail.com>
2024-11-02 16:37:41 -0400

    Get rid of modules that are no longer supported/required

--- modules/iam-policy/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "role_arn" {
-  description = "The ARN of the IAM Role"
-  value       = aws_iam_role.iam_role.arn
-}
-
-output "role_name" {
-  description = "The name of the IAM Role"
-  value       = aws_iam_role.iam_role.name
-}
