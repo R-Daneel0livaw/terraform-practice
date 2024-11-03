58d92de470586f0085b60376d24f9e90b401cfab
erich <eric.hine2214@gmail.com>
2024-11-03 15:57:07 -0500

    Add initial lambda function for bucket

---
+++ modules/lambda/lambda_code/bucket1_lambda1.py
@@ -0,0 +1,10 @@
+import os
+def lambda_handler(event, context):
+    bucket_name = os.getenv('BUCKET_NAME', 'No bucket name set')
+    
+    print(f"The bucket name is: {bucket_name}")
+    
+    return {
+        'statusCode': 200,
+        'body': f"The bucket name is: {bucket_name}"
+    }
