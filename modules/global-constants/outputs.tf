71ebb8cda3157553be026723621bea993bfcab43
erich <eric.hine2214@gmail.com>
2024-11-06 17:45:24 -0500

    Add bucket name constants for other buckets in pipeline

--- modules/global-constants/outputs.tf
+++ modules/global-constants/outputs.tf
@@ -3,6 +3,16 @@ output "build_raw_data_bucket_name" {
   value       = "build-raw-data"
 }
 
+output "build_preprocessed_data_bucket_name" {
+  description = "The name of the S3 bucket"
+  value       = "build-preprocessed-data"
+}
+
+output "build_complete_data_bucket_name" {
+  description = "The name of the S3 bucket"
+  value       = "build-compete-data"
+}
+
 output "directories" {
   value = ["inbound", "waiting", "completed", "archived"]
 }
