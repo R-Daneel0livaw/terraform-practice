5e4a13268a24d5c19a12f64972e52e0276ed5a69
erich <eric.hine2214@gmail.com>
2025-02-11 17:56:43 -0500

    Fix foundation resource outputs

--- projects/project1/test/foundation/outputs.tf
+++ projects/project1/test/foundation/outputs.tf
@@ -1,7 +1,7 @@
 output "bucket_info" {
   description = "An object containing the bucket name, id, and arn"
   value = {
-    bucket = module.build_raw_data_bucket.bucket
+    bucket = module.build_raw_data_bucket.bucket_name
     id     = module.build_raw_data_bucket.bucket_id
     arn    = module.build_raw_data_bucket.bucket_arn
   }
@@ -14,7 +14,7 @@ output "lambda_role_arn" {
 output "sqs_queue_arns" {
   description = "ARNs of the SQS queues"
   value = {
-    waiting = module.waiting_sqs_queue.arn
-    completed = module.completed_sqs_queue.arn
+    waiting = module.waiting_sqs_queue.queue_arn
+    completed = module.completed_sqs_queue.queue_arn
   }
 }
